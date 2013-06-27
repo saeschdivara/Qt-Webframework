b61511c7862ad1b52aa8327d3098e0e5fdcda6fb
Sascha Häusler <saeschdivara@gmail.com>
2013-06-27 19:56:20 +0200

    Layer for sessions added

--- src/website/WebSession.h
+++ src/website/WebSession.h
@@ -5,6 +5,8 @@
 #include <httpserverresponse.h>
 #include <sessionstore.h>
 
+#include <QtCore/QVariant>
+
 namespace web {
 namespace website {
 
@@ -18,6 +20,26 @@ class WebSession
             m_response(response),
             m_store(store)
         {
+        }
+
+        ~WebSession() {
+            m_store->removeSession(*m_request, *m_response);
+        }
+
+        inline void setProperty(const QByteArray &key, const QVariant &value) {
+            m_store->setProperty(*m_request, *m_response, key, value);
+        }
+
+        inline QVariant getProperty(const QByteArray &key) const {
+            return m_store->property(*m_request, *m_response, key);
+        }
+
+        inline void removeProperty(const QByteArray &key) {
+            m_store->removeProperty(*m_request, *m_response, key);
+        }
+
+        inline QList<QByteArray> properties() const {
+            return m_store->properties(*m_request, *m_response);
         }
 
     private:

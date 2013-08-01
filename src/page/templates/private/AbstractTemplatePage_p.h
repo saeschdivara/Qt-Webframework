9d4319fffbb2fb498a076e315d7901ea2f76c251
Saesch di Vara <saeschdivara@gmail.com>
2013-08-01 15:44:48 -0300

    Added two small helper methods and expanded the documentation

--- src/page/templates/private/AbstractTemplatePage_p.h
+++ src/page/templates/private/AbstractTemplatePage_p.h
@@ -24,6 +24,12 @@ namespace page
 namespace templates
 {
 
+/**
+ * @brief The AbstractTemplatePagePrivate class
+ *
+ * @author Sascha Häusler <saeschdivara@gmail.com>
+ * @since 0.2
+ */
 class AbstractTemplatePagePrivate
 {
     public:
@@ -45,7 +51,42 @@ class AbstractTemplatePagePrivate
         model::AbstractModel *pageModel;
         QHash<QString, model::AbstractListModel *> templateModels;
 
+        /**
+         * @brief Checks if there were any post variables in the request
+         *
+         * @return Returns true if the request had any post variables
+         *
+         * @author Sascha Häusler <saeschdivara@gmail.com>
+         * @since 0.2
+         */
+        bool hasPostVariables() {
+            return post.isEmpty();
+        }
 
+        /**
+         * @brief Checks if there were any get variables in the request
+         *
+         * @return Returns true if the request had any get variables
+         *
+         * @author Sascha Häusler <saeschdivara@gmail.com>
+         * @since 0.2
+         */
+        bool hasGetVariables() {
+            return get.isEmpty();
+        }
+
+        /**
+         * @brief This method replaces every placeholder which has to be like this:
+         * $place_holder_name$. The placeholdername has to match a Qt object property.
+         * Thanks to the property method the method replaces every placeholder, if they don't
+         * exists with an empty space.
+         *
+         * @param pageContent   The content which contains the template in which the placeholders are replaced
+         * @param model         Model which holds the properties for the template
+         *
+         * @author Sascha Häusler <saeschdivara@gmail.com>
+         * @since 0.2
+         */
         void replaceModelPlaceholders(QString &pageContent, web::page::model::AbstractModel *model) {
             QRegularExpression regex("\\$(.*)\\$");
             QRegularExpressionMatchIterator matchIterator = regex.globalMatch(pageContent);
@@ -59,6 +100,20 @@ class AbstractTemplatePagePrivate
                 }
         }
 
+        /**
+         * @brief This method enables to either use a model property or a real
+         * value for a specific template attribute and decide which as which
+         * type it is returned.
+         *
+         * @param ele   Dom element which holds the template informations
+         * @param attr  Attribute name of the dom element
+         * @param model Model which could hold the property for the attribute
+         *
+         * @return Returns the dom attribute as the wanted type
+         *
+         * @author Sascha Häusler <saeschdivara@gmail.com>
+         * @since 0.2
+         */
         template<class T>
         T getTemplateAttribute(QDomElement & ele, const QString & attr, web::page::model::AbstractModel *model) {
             QString domAttribute = ele.attribute(attr);
@@ -70,6 +125,18 @@ class AbstractTemplatePagePrivate
             return QVariant(domAttribute).value<T>();
         }
 
+        /**
+         * @brief Through the model attribute it can be decided if the user
+         * can see what the template would generate or not.
+         *
+         * @param ifAttribute   Model attribute which has to be either true or false
+         * @param model         Model which holds the attribute
+         *
+         * @return If the model has a valid attribute it will be returned, otherwise false
+         *
+         * @author Sascha Häusler <saeschdivara@gmail.com>
+         * @since 0.2
+         */
         bool isTemplateAllowed(QString ifAttribute, web::page::model::AbstractModel *model) {
             QVariant ifProperty = model->property(ifAttribute.toUtf8().data());
             if (ifProperty.isValid()) {
